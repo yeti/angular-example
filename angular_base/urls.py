94f570517e4b895ec721a8b0982d44df2c215f2e
Alex <alex@yetihq.com>
2014-10-31 16:02:28 -0700

    changed index and urls

--- angular_base/urls.py
+++ angular_base/urls.py
@@ -8,6 +8,5 @@ urlpatterns = patterns('',
     # This is our new URL
     url(r'^example/$', 'angular_example.views.example', name="example"),
     url(r'^$', 'baseclone.views.index', name="index"),
-    # url(r'^proxy/(?P<path>.*)$', 'baseclone.views.proxy_to', {'target_url': 'https://basecamp.com/2780377/api/v1/'}),
-    url(r'^proxy/(?P<path>.*)$', 'baseclone.views.proxy_to', {'target_url': 'https://basecamp.com/[your_basecamp_id_here]/api/v1/'}),
+    url(r'^proxy/(?P<path>.*)$', 'baseclone.views.proxy_to', {'target_url': 'https://basecamp.com/your_basecamp_id_here/api/v1/'}),
 )
