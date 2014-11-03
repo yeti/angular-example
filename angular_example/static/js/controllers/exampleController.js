aaf847f1767acd9f0fc6ab5b69a77dca5e07e945
Alex <alex@yetihq.com>
2014-11-02 18:52:05 -0800

    modified example

--- angular_example/static/js/controllers/exampleController.js
+++ angular_example/static/js/controllers/exampleController.js
@@ -1,21 +1,16 @@
 function exampleController($scope, $http) {
-    $scope.stuff = "I have a default value";
-
-    $scope.firstName = "Rudy";
-    $scope.lastName = "Mutter";
-
     $scope.chuckNorrisMe = function(){
         $http({method: 'GET',
-            url: 'http://api.icndb.com/jokes/random?firstName=' + $scope.firstName + '&lastName=' + $scope.lastName}).
+            url: 'http://api.icndb.com/jokes/random?firstName=Chuck&lastName=Norris'}).
             success(function(data, status, headers, config) {
                 // this callback will be called asynchronously
                 // when the response is available
-                $scope.chuckNorris = data.value.joke;
+                $scope.chuckNorrisJoke = data.value.joke;
             }).
             error(function(data, status, headers, config) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
-                $scope.chuckNorris = $scope.firstName + $scope.lastName + " never fails";
+                $scope.chuckNorrisJoke = "Chuck Norris never fails";
             });
     };
 
--- angular_example/views.py
+++ angular_example/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 __author__ = 'baylee'
 
+
 def example(request):
     return render(request, "example.html")
